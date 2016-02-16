fe0015cc74d0df826690d46f4afe8d7ed2986179
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-16 12:50:30 -0500

    orm.js file, statred controllers to get routes

--- burger/models/burger.js
+++ burger/models/burger.js
@@ -3,3 +3,5 @@
  //        4. Inside `burger.js` create the code that will call the orm functions using burger specific input for the orm
 
  //        5. Export at the end of the `burger.js` file
+
+ var orm = require("../config/orm.js");
