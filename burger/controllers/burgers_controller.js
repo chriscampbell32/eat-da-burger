261949e72f2865a48be724cd689f3dceb8cb8335
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-16 13:31:03 -0500

    struggling with a bug...

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -1,3 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger.js');
+var app = express();
+
+
+
+//create routes
+router.get('/', function(req, res) {
+  actions.showBurgers(function(burger_data) {
+    res.render('index', {burger_data});
+  });
+});
+
+router.post("/create", function(req, res) {
+  actions.addBurger(req.body.entry, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+ 
+
+module.exports = router;
+
--- burger/models/burger.js
+++ burger/models/burger.js
@@ -4,4 +4,22 @@
 
  //        5. Export at the end of the `burger.js` file
 
- var orm = require("../config/orm.js");
+var orm = require("../config/orm.js");
+
+
+
+var burgers = {
+  addBurger: function(nameInput, cb){
+    orm.insertOne("burgers", nameInput, cb);
+  },
+  devourBurger: function(inputId, cb){
+    orm.updateOne("burgers", inputId, cb);
+  },
+  findAll: function(cb) {
+    orm.selectAll("burgers", function(res){
+      cb(res);
+    });
+  }
+};
+
+module.exports = burgers;
