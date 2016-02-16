fd51eb344d5d9cbcc5f73599260f5d9b7f2baf05
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-15 21:41:57 -0500

    models folder created

---
+++ burger/models/burger.js
@@ -0,0 +1,5 @@
+ // 3. Inside `burger.js`, import `orm.js` into `burger.js`
+
+ //        4. Inside `burger.js` create the code that will call the orm functions using burger specific input for the orm
+
+ //        5. Export at the end of the `burger.js` file
